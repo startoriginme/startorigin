5cf8a0509b94395a225d8554825eef31f896ff50
startoriginme <mnklv.mail@gmail.com>
2025-11-09 12:13:19 +0100

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/lightbulbb.png" type="image/png" />
+      <link rel="shortcut icon" href="/lightbulbb.png" type="image/png" />
+      <link rel="apple-touch-icon" href="/lightbulbb.png" />
+    </>
+  )
+}
