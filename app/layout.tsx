ebfc700b144500d734214ceda3a2e5c19b6a3648
startoriginme <mnklv.mail@gmail.com>
2025-11-09 12:10:13 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,9 @@ export const metadata: Metadata = {
   description: "A platform for innovators to share problems and collaborate on solutions",
   generator: 'v0.app',
   icons: {
-    icon: "/lightbulb.png",
-    shortcut: "/lightbulb.png",
-    apple: "/lightbulb.png",
+    icon: "/lightbulbb.png",
+    shortcut: "/lightbulbb.png",
+    apple: "/lightbulbb.png",
   },
 }
 
