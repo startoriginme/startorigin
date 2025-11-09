7c5e0135b1e68402acb3859a635af82a8212ac9e
startoriginme <mnklv.mail@gmail.com>
2025-11-09 12:34:39 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="/lightbulbb.png" type="image/png" />
-      <link rel="shortcut icon" href="/lightbulbb.png" type="image/png" />
-      <link rel="apple-touch-icon" href="/lightbulbb.png" />
+      <link rel="icon" href="https://startorigin.netlify.app/lightbulbb.png" type="image/png" />
+      <link rel="shortcut icon" href="https://startorigin.netlify.app/lightbulbb.png" type="image/png" />
+      <link rel="apple-touch-icon" href="https://startorigin.netlify.app/lightbulbb.png" />
     </>
   )
 }
